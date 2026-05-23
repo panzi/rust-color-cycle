659993a6fc69756a145b8242aafec86ae1704b10
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2026-05-23 04:10:15 +0200

    better input_test.py

--- input_test.py
+++ input_test.py
@@ -1,11 +1,26 @@
 #!/usr/bin/env python3
 
 import os
-import sys
 import termios
 
+C_IFLAG = 0
+C_OFLAG = 1
+C_CFLAG = 2
+C_LFLAG = 3
+C_LINE  = 4
+C_CC    = 6
+
+VTIME = 5
+VMIN  = 6
+
 ttyinfo = termios.tcgetattr(0)
-ttyinfo[3] &= ~(termios.ICANON | termios.ECHO)
+print(ttyinfo)
+ttyinfo[C_IFLAG] |= ~(termios.BRKINT | termios.ICRNL | termios.INPCK | termios.ISTRIP | termios.IXON)
+ttyinfo[C_OFLAG] |= termios.ONLCR
+ttyinfo[C_LFLAG] |= termios.CS8
+ttyinfo[C_LFLAG] &= ~(termios.ECHO | termios.ICANON | termios.IEXTEN | termios.ISIG)
+ttyinfo[C_CC][VMIN]  = 0
+ttyinfo[C_CC][VTIME] = 0
 termios.tcsetattr(0, termios.TCSANOW, ttyinfo)
 
 try:
