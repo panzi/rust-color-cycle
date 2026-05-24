5678bc9d32f56ed64a2093dc223609230f56382a
Mathias Panzenböck <grosser.meister.morti@gmx.net>
2026-05-24 03:53:06 +0200

    input_test.py: debug mouse events

--- input_test.py
+++ input_test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import os
+import sys
+import select
 import termios
 
 C_IFLAG = 0
@@ -13,8 +15,11 @@ C_CC    = 6
 VTIME = 5
 VMIN  = 6
 
-ttyinfo = termios.tcgetattr(0)
-print(ttyinfo)
+orig_ttyinfo = termios.tcgetattr(0)
+print(orig_ttyinfo)
+ttyinfo = orig_ttyinfo.copy()
+ttyinfo[C_CC] = orig_ttyinfo[C_CC].copy()
+
 ttyinfo[C_IFLAG] |= ~(termios.BRKINT | termios.ICRNL | termios.INPCK | termios.ISTRIP | termios.IXON)
 ttyinfo[C_OFLAG] |= termios.ONLCR
 ttyinfo[C_LFLAG] |= termios.CS8
@@ -23,15 +28,39 @@ ttyinfo[C_CC][VMIN]  = 0
 ttyinfo[C_CC][VTIME] = 0
 termios.tcsetattr(0, termios.TCSANOW, ttyinfo)
 
+# enable mouse tracking (click and move)
+sys.stdout.write('\x1B[?1000h')
+sys.stdout.write('\x1B[?1003h')
+sys.stdout.write('\x1B[?1006h')
+sys.stdout.flush()
+
 try:
     fp = os.fdopen(0, "rb", buffering=0)
-    while True:
-        buf = fp.read()
-        if buf:
+    epoll = select.epoll(flags=select.EPOLL_CLOEXEC)
+    epoll.register(0, select.EPOLLIN | select.EPOLLRDHUP)
+
+    running = True
+    while running:
+        has_data = False
+
+        for efd, eflags in epoll.poll():
+            if eflags & select.EPOLLRDHUP:
+                running = False
+
+            if eflags & select.EPOLLIN:
+                has_data = True
+
+        if has_data and (buf := fp.read()) and buf:
             print(repr(buf))
+            if buf == b'\x1B' or buf == b'q':
+                break
+
 except KeyboardInterrupt:
     print("^C")
 
 finally:
-    ttyinfo[3] |= termios.ICANON | termios.ECHO
-    termios.tcsetattr(0, termios.TCSANOW, ttyinfo)
+    # disable mouse tracking (click and move)
+    sys.stdout.write('\x1B[?1001l')
+    sys.stdout.flush()
+
+    termios.tcsetattr(0, termios.TCSANOW, orig_ttyinfo)
